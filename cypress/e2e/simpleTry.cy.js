e6eca60a7c41cc801cab9b8500f37e90e8490182
Odysseas Sofikitis <o.sofikitis@gmail.com>
2023-12-21 23:31:59 +0200

    Writes test for brocolli product increment

--- cypress/e2e/simpleTry.cy.js
+++ cypress/e2e/simpleTry.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Visit site and click some buttons', () => {
+  before(() => {
+    cy.visit('/')
+  })
+
+  it('Add vegetable', () => {
+    cy.contains('.product-name', 'Brocolli - 1 Kg')
+      .parent('.product')
+      .find('.increment')
+      .click();
+
+    cy.contains('.product-name', 'Brocolli - 1 Kg')
+      .parent('.product')
+      .find('.quantity')
+      .should('have.value', '2');
   })
 })
